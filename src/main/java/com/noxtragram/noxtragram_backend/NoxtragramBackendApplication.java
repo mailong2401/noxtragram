57313c51aa06179aa447b40d6c400ca771c4fe50
mailong2401 <gtmailong@gmail.com>
2025-09-24 15:46:52 +0700

    create project backend noxtragram

---
+++ src/main/java/com/noxtragram/noxtragram_backend/NoxtragramBackendApplication.java
@@ -0,0 +1,13 @@
+package com.noxtragram.noxtragram_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoxtragramBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoxtragramBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noxtragram/noxtragram_backend/NoxtragramBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noxtragram.noxtragram_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoxtragramBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
